36b57f88b3e90c4e37ba3346165835c9ed6250f0
137411244 <137411244@qq.com>
2017-12-26 15:39:35 +0800

    星期二下午

--- app/src/main/java/cn/edu/gdmec/android/boxuegu/adapter/ExercisesDetailAdapter.java
+++ app/src/main/java/cn/edu/gdmec/android/boxuegu/adapter/ExercisesDetailAdapter.java
@@ -1,8 +1,63 @@
 package cn.edu.gdmec.android.boxuegu.adapter;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cn.edu.gdmec.android.boxuegu.R;
+import cn.edu.gdmec.android.boxuegu.bean.ExercisesBean;
+
 /**
  * Created by student on 17/12/26.
  */
 
-public class ExercisesDetailAdapter {
+public class ExercisesDetailAdapter extends BaseAdapter {
+    private Context mContext;
+    private List<ExercisesBean> ebl;
+    private OnSelectListener onSelectListener;
+    public ExercisesDetailAdapter(Context context,OnSelectListener onSelectListener){
+        this.mContext = context;
+        this.onSelectListener = onSelectListener;
+    }
+    public void setData(List<ExercisesBean> ebl){
+        this.ebl = ebl;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public int getCount() {
+        return ebl == null ? 0 : ebl.size();
+    }
+
+    @Override
+    public ExercisesBean getItem(int position) {
+        return ebl == null ? null : ebl.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+    private ArrayList<String>selectedPosition = new ArrayList<String>();
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        final ViewHolder vh;
+        if (convertView == null){
+            vh = new ViewHolder();
+            convertView = LayoutInflater.from(mContext).inflate(
+                    R.layout.exercises_detail_list_item,null);
+            vh.subject = (TextView)convertView.findViewById(R.id.tv_subject);
+
+
+            )
+        }
+    }
 }
