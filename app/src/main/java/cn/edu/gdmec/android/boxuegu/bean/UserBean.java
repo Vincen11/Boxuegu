066636cba8caa6de98a040d94e1bd607a9bebeaf
Vincen11 <2582990780@qq.com>
2017-12-29 10:44:11 +0800

    2.4

---
+++ app/src/main/java/cn/edu/gdmec/android/boxuegu/bean/UserBean.java
@@ -0,0 +1,12 @@
+package cn.edu.gdmec.android.boxuegu.bean;
+
+/**
+ * Created by student on 17/12/29.
+ */
+
+public class UserBean {
+    public String userName;
+    public String nickName;
+    public String sex;
+    public String signature;
+}
